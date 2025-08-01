d6cad6bd808b944e3f820835a2d4c2ada12d3925
CroKodila <kakunin.k.i@gmail.com>
2025-08-02 00:47:35 +0300

    fixed library version conflicts & add README

--- server.cjs
+++ server.cjs
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 
 const app = express();
-app.use(express.static('./dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (request, response) =>{
-    response.sendFile(path.join(__dirname, './dist/index.html'))
+    response.sendFile(path.join(__dirname, 'dist', 'index.html'))
     response.status(200)
 })
 
